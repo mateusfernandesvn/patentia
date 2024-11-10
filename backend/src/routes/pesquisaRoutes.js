6029ff281e704de74b3759e904c60f05a8485ac4
Jeferson Oliveira <jeferson_phx@live.com>
2024-11-10 00:32:08 -0300

    Ajuste na rota pesquisar

--- backend/src/routes/pesquisaRoutes.js
+++ backend/src/routes/pesquisaRoutes.js
@@ -4,6 +4,6 @@ const { executarPesquisa } = require('../controllers/pesquisaController');
 const router = express.Router();
 
 // Rota POST para executar a pesquisa
-router.post('/pesquisar', executarPesquisa);
+router.post('/', executarPesquisa);
 
 module.exports = router;
