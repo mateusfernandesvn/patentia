cf619c3481659b1e26d8baa63824484878c33e27
Jeferson Oliveira <jeferson_phx@live.com>
2024-11-14 00:07:04 -0300

    Remoção da rota e controller historico sem uso no projeto

--- backend/src/routes/historicoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { obterHistorico } = require('../controllers/historicoController');
-
-const router = express.Router();
-
-// Rota GET para executar a pesquisa
-router.get('/', obterHistorico);
-
-module.exports = router;
